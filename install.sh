59e3fa0cfe0a8359149e56fd82bd1310d321e287
rainer <dvb@itnt.me>
2017-04-30 13:22:11 +0400

    fix install script

--- install.sh
+++ install.sh
@@ -5,3 +5,11 @@ npm install
 
 cd `dirname $0`/static
 bower install
+
+go get github.com/gorilla/websocket && \
+go get github.com/jinzhu/gorm && \
+go get github.com/jinzhu/gorm/dialects/mysql && \
+go get github.com/mqu/openldap && \
+go get golang.org/x/net/xsrftoken && \
+go get gopkg.in/gin-gonic/gin.v1
+
